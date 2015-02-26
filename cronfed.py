9899e492e65fbad0f84b56449bf367d431701f87
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-02-25 19:10:15 -0800

    transferring over all the work to make the feed valid and IFTTT compatible from weeklypedia. if you're interested in what that work was, check out commits f92dbde6fdd9a75d09189fa2c28783c98e3c9f17 through 44648731ec35175a40c44d7ba884f9c3995d3c64 in github.com/hatnote/weeklypedia

--- cronfed.py
+++ cronfed.py
@@ -1,11 +1,22 @@
+
+# Credit to Mark Williams for this
+import re
+import uuid
+import socket
+import mailbox
 import argparse
+import tempfile
 from contextlib import closing
 from collections import namedtuple
 import xml.etree.cElementTree as ET
-import mailbox
-import tempfile
-import uuid
-import re
+
+
+FEED_TITLE = 'Cronfed'
+DEFAULT_LINK = 'http://hatnote.com'
+DEFAULT_DESC = 'Fresh cron output from cronfed'
+DEFAULT_TITLE = 'Cronfed on %s' % socket.gethostname()
+EXCLUDE_TAGS = set(['lastBuildDate'])
+GUID_URL_TMPL = 'http://hatnote.com/{guid}'
 
 
 class mbox_readonlydir(mailbox.mbox):
@@ -107,17 +118,29 @@ class mbox_readonlydir(mailbox.mbox):
 
 DEFAULT_SUBJECT_PARSER = re.compile(
     'Cron <(?P<user>[^@].+)@(?P<host>[^>].+)> (?P<command>.*)')
-DEFAULT_SUBJECT_RENDERER = 'Cron <%(user)s@%(host)s> %(command)s'
+DEFAULT_SUBJECT_RENDERER = 'cron: <%(user)s@%(host)s> %(command)s'
 
 
 MESSAGE_ID_PARSER = re.compile('<(?P<id>[^@]+)@(?P<host>[^>+]+)>')
 
 
-class RSSItem(namedtuple('RSSItem', ['title', 'description', 'link',
-                                     'lastBuildDate', 'pubDate', 'guid'])):
+def find_python_error_type(text):
+    from tbutils import ParsedTB
+    try:
+        tb_str = text[text.index('Traceback (most recent'):]
+    except ValueError:
+        raise ValueError('no traceback found')
+    parsed_tb = ParsedTB.from_string(tb_str)
+    return parsed_tb.exc_type
 
+
+BaseRSSItem = namedtuple('RSSItem', ['title', 'description', 'link',
+                                     'lastBuildDate', 'pubDate', 'guid'])
+
+
+class RSSItem(BaseRSSItem):
     @classmethod
-    def fromemail(cls, email, excludes=('command',),
+    def fromemail(cls, email, excludes=(),
                   redacted='REDACTED',
                   parser=DEFAULT_SUBJECT_PARSER,
                   renderer=DEFAULT_SUBJECT_RENDERER):
@@ -136,17 +159,56 @@ class RSSItem(namedtuple('RSSItem', ['title', 'description', 'link',
             guid = uuid.uuid4()
         else:
             guid = match.group('id')
+        body = email.get_payload()
 
-        return cls(title=title, description=None, link=None,
+        desc = 'Cron ran %s at %s.' % (parsed['command'], pubDate)
+        try:
+            python_error_type = find_python_error_type(body)
+        except:
+            python_error_type = None
+        if python_error_type:
+            desc += ' Check for a Python exception: %s.' % python_error_type
+
+        if body:
+            desc += ' Command output:\n\n' + summarize(body, 16)
+
+        return cls(title=title, description=desc, link=None,
                    lastBuildDate=lastBuildDate, pubDate=pubDate, guid=guid)
+
+
+def summarize(text, length):
+    """
+    Length is the amount of text to show. It doesn't include the
+    length that the summarization adds back in."
+    """
+    len_diff = len(text) - length
+    if len_diff <= 0:
+        return text
+    return ''.join([text[:length/2],
+                    '... (%s bytes) ...' % len_diff,
+                    text[-length/2:]])
 
 
 def render_rss(rss_items):
     rss = ET.Element('rss', {'version': '2.0'})
     channel = ET.SubElement(rss, 'channel')
+    title_elem = ET.SubElement(channel, 'title')
+    title_elem.text = DEFAULT_TITLE
+    desc_elem = ET.SubElement(channel, 'description')
+    desc_elem.text = DEFAULT_DESC
+    link_elem = ET.SubElement(channel, 'link')
+    link_elem.text = DEFAULT_LINK
+
     for rss_item in rss_items:
         item = ET.SubElement(channel, 'item')
         for tag, text in rss_item._asdict().items():
+            if tag in EXCLUDE_TAGS:
+                continue
+            if tag == 'link' and text is None:
+                text = DEFAULT_LINK
+            if tag == 'guid':
+                text = GUID_URL_TMPL.format(guid=text)
+                # elem = ET.SubElement(item, tag, {'isPermaLink': 'false'})
             elem = ET.SubElement(item, tag)
             elem.text = text
 
